959d2659cccac1c2fccdf93ca6141bf1515056f5
Abdülhamid Uysal <hamituysal25@gmail.com>
2024-12-07 15:34:38 +0300

    PostgresSql baglandı
    bağlantıyı test etmek için form 2 oluşturdum , önemsiz

---
+++ Blaze/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Blaze
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        NpgsqlConnection baglanti = new NpgsqlConnection("server = localHost; port = 5432; Database = SteamClone;" +
+            " user ID = postgres; password = 123"); 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * From Games";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu,baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
